e9b2b3d6f403a8ad754db0f28ef91e58b604bec9
Alexander Heavens <alexander.heavens@gmail.com>
2020-04-04 21:48:40 +0000

    Add VSCode extensions

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 user="${HOST_USER_NAME}"
-useradd -u ${HOST_USER_ID} ${user}
-mv /home/user /home/${user}
-chown ${user}:${user} /home/${user}
+useradd --uid ${HOST_USER_ID} ${user}
+mv -t /home/${user} /home/user/.config /home/user/.vscode
+chown -R ${user}:${user} /home/${user}
 
-su ${user}
-cd
+bash
--- scripts/entrypoint.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-user="${HOST_USER_NAME}"
-useradd -u ${HOST_USER_ID} ${user}
-mv /home/user /home/${user}
-chown ${user}:${user} /home/${user}
-
-su ${user}
-exec sh
