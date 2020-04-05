d5494d930521e995fdb41b1919e3b706f5dc6613
Alexander Heavens <alexander.heavens@gmail.com>
2020-04-05 09:25:52 +0000

    Avoid host volume changes

--- docker/entrypoint.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-user="${HOST_USER_NAME}"
-useradd --uid ${HOST_USER_ID} ${user}
-mv -t /home/${user} /home/user/.config /home/user/.vscode
-chown -R ${user}:${user} /home/${user}
-
-bash
